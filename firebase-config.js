1eb15a18815b060e71ac458db52c3d662ebe69e2
Mellisa Belita Sialumba <150239598+beltasia@users.noreply.github.com>
2025-04-17 13:51:16 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,12 @@
 // Firebase configuration (replace with your actual config)
 const firebaseConfig = {
-    apiKey: "AIzaSyCyw6LxuauzyPHHtLs7mjeyrauY6FmkLRk",
-    authDomain: "equipment-tracking-4cc1a.firebaseapp.com",
-    projectId: "equipment-tracking-4cc1a",
-    storageBucket: "equipment-tracking-4cc1a.firebasestorage.app",
-    messagingSenderId: "648095088927",
-    appId: "1:648095088927:web:46d065731b0d6bfd27702a",
-    measurementId: "G-JX9LZNG2KJ"
+    apiKey: "*******************************",
+    authDomain: "****************************",
+    projectId: "*************",
+    storageBucket: "*************",
+    messagingSenderId: "*******************",
+    appId: "***********************************",
+    measurementId: "***********************"
   };
   
   // Initialize Firebase
